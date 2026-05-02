a628d7f957189ce8987e8efab15a2ee6c9f80ef1
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-05-02 19:38:02 +0100

    Add constants for tab configuration and icon URLs
    - Created a new file `data.ts` to define tab configurations including names, titles, and associated icons.
    - Introduced a new file `icons.ts` to store the URLs for various icons used in the application.

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,13 +1,60 @@
+import { tabs } from "@/constants/data";
 import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TabsLayout() {
+  const insets = useSafeAreaInsets();
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="insights" options={{ title: "Insights" }} />
-      <Tabs.Screen name="subscriptions" options={{ title: "Subscription" }} />
-      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: Math.max(insets.bottom, 20),
+          height: 70,
+          marginHorizontal: 20,
+          borderRadius: 32,
+          backgroundColor: "#111",
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        tabBarItemStyle: {
+          paddingVertical: 5,
+        },
+        tabBarIconStyle: {
+            width: 48,
+            height: 48,
+            alignItems: "center",
+          }
+      }}
+    >
+      {tabs.map((tab) => (
+        <Tabs.Screen
+          key={tab.name}
+          name={tab.name}
+          options={{
+            title: tab.title,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon focused={focused} icon={tab.icon} />
+            ),
+          }}
+        />
+      ))}
       <Tabs.Screen name="subscriptions/[id]" options={{ href: null }} />
-  </Tabs>
+    </Tabs>
+  );
+}
+
+function TabIcon({ focused, icon }: { focused: boolean; icon: string }) {
+  return (
+    <View className="size-12 items-center justify-center">
+      <View
+        className={`size-12 items-center justify-center rounded-full bg-transparent ${focused && "#fff"}`}
+      >
+        <Image src={icon} resizeMode="contain" className="size-6" />
+      </View>
+    </View>
   );
 }
---
+++ frontend/constants/data.ts
@@ -0,0 +1,8 @@
+import { iconsUrl } from "./icons";
+
+export const tabs = [
+  { name: "index", title: "Home", icon: iconsUrl.home },
+  { name: "insights", title: "Insights", icon: iconsUrl.activity },
+  { name: "subscriptions", title: "Subscription", icon: iconsUrl.wallet },
+  { name: "settings", title: "Settings", icon: iconsUrl.setting },
+];
