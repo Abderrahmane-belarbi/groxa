fe61b118cfa17402b2f73e4d79dd26c6db1947e9
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-05-03 09:21:29 +0100

    feat: add custom fonts and update layout to support font loading

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -5,7 +5,8 @@ export default function Index() {
 
   return (
     <SafeAreaView className="flex-1 justify-center items-center" >
-      <Text className="text-2xl font-bold">Welcome to Groxa!</Text>
+      <Text className="text-2xl font-extrabold">Welcome to Groxa!</Text>
+      <Text className="text-2xl font-sans-extrabold">Welcome to Groxa!</Text>
       <View className="gap-2 my-4">
         <Link href={"/(auth)/sign-in"}>Login</Link>
         <Link href={"/(auth)/sign-up"}>Sign up</Link>
