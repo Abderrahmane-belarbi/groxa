1fe0d26e9d9a9b3061b561b5c294109ab1f3ebed
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-05-04 19:04:30 +0100

    creating balance card

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,15 +1,9 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import BalanceCard from "@/components/home/balance-card";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
-
   return (
-    <SafeAreaView className="flex-1 justify-center items-center" >
-      <Text className="text-2xl font-sans-extrabold">Welcome to Groxa!</Text>
-      <View className="gap-2 my-4">
-        <Link href={"/(auth)/sign-in"}>Login</Link>
-        <Link href={"/(auth)/sign-up"}>Sign up</Link>
-      </View>
+    <SafeAreaView className="flex-1 items-center" >
+      <BalanceCard />
     </SafeAreaView>
   );
 }
---
+++ frontend/components/home/balance-card.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function BalanceCard() {
+  return (
+    <View className="bg-[#EA7A53] p-4 rounded-tr-lg rounded-bl-lg">
+      <Text className="text-2xl font-sans-extrabold bg-black text-white">Balance Card</Text>
+    </View>
+  );
+}
