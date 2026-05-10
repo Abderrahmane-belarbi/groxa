37517bc92c1183852f603c8b12fdda66561857d4
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-05-10 21:08:04 +0100

    feat: add email verification process with token generation and user model updates

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -3,6 +3,8 @@ import { registerSchema } from "../lib/validations/auth.validation";
 import { z } from "zod";
 import { User } from "../models/user.model";
 import bcrypt from "bcryptjs";
+import { generateVerificationToken, generateVerificationTokenExpiresAt } from "../utils/generate-verification-token";
+import { generateTokenSetCookie } from "../utils/generate-token-cookie";
 
 export async function register(req: Request, res: Response) {
   const { name, email, password } = req.body;
@@ -24,14 +26,16 @@ export async function register(req: Request, res: Response) {
       return res.status(409).json({ message: "Email is already registered" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = generateVerificationToken();
+    const verificationTokenExpiresAt = generateVerificationTokenExpiresAt();
 
     const newUser = await User.create({
       name,
       email: normalizedEmail,
       password: hashedPassword,
       role: "user",
-      emailVerified: false,
-      twoFactorEnabled: false,
+      emailVerificationToken: verificationToken,
+      emailVerificationTokenExpires: verificationTokenExpiresAt,
     });
 
     res.status(201).json({ message: "User registered successfully", user: newUser });
@@ -39,5 +43,53 @@ export async function register(req: Request, res: Response) {
   } catch (error) {
     console.error("Error in register controller:", error);
     res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function verificationEmail(req: Request, res: Response) {
+  try {
+    const { code, email } = req.body;
+    if (!code || !email)
+      return res.status(400).json({ message: "Required fields are missing" });
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Please Signup first" });
+
+    if (user.emailVerified)
+      return res.status(410).json({ message: "Email already verified" });
+
+    if (!user.emailVerificationToken || !user.emailVerificationTokenExpires) {
+      return res.status(400).json({ message: "No active verification token" });
+    }
+
+    if (String(user.emailVerificationToken) !== String(code))
+      return res.status(400).json({ message: "Invalid verification code" });
+
+    if (Date.now() > new Date(user.emailVerificationTokenExpires).getTime()) {
+      return res.status(400).json({ message: "Token expired" });
+    }
+
+    user.emailVerified = new Date();
+    user.emailVerificationTokenExpires = undefined;
+    user.emailVerificationToken = undefined;
+
+    await user.save();
+
+    // jwt
+    generateTokenSetCookie(res, user._id);
+
+    return res.status(200).json({
+      message: "The email has been verified successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        emailVerified: user.emailVerified ? true : false,
+      },
+    });
+  } catch (error) {
+    let message = "Internal server error";
+    error instanceof Error && (message = error.message);
+    return res.status(500).json({ message });
   }
 }
